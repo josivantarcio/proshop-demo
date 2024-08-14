7731a94bcf8d8a6fe95d764c48a8240adf04d0c0
Josevan Oliveira <josivantarcio@msn.com>
2024-08-14 10:58:45 -0300

    create product.jsx HomeScreen.jsx

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'A tecnologia Bluetooth permite que você conecte com dispositivos compatíveis sem fio. Áudio de alta qualidade AAC oferece uma experiência de escuta imersiva. Microfone embutido permite que você atenda chamadas enquanto trabalha.',
+    brand: 'Apple',
+    category: 'Eletronicos',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Apresentando o iPhone 11 Pro. Um sistema de câmera tripla transformador que adiciona muitas capacidades sem complexidade. Um salto sem precedentes na duração da bateria.',
+    brand: 'Apple',
+    category: 'Eletronicos',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Caracterizada por especificações de imagem versáteis, a Canon EOS 80D se destaca ainda mais usando um par de sistemas de foco robustos e um design intuitivo.',
+    brand: 'Cannon',
+    category: 'Eletronicos',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'O centro de entretenimento doméstico definitivo começa com o PlayStation. Seja para jogos, filmes em HD, televisão, música.',
+    brand: 'Sony',
+    category: 'Eletronicos',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Tenha um melhor controle dos seus jogos com este mouse para jogos Logitech LIGHTSYNC. Os seis botões programáveis permitem personalização para uma experiência de jogo suave.',
+    brand: 'Logitech',
+    category: 'Eletronicos',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Conheça o Echo Dot - Nosso alto-falante inteligente mais popular com um design em tecido. É o nosso alto-falante inteligente mais compacto, que se encaixa perfeitamente em pequenos espaços.',
+    brand: 'Amazon',
+    category: 'Eletronicos',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
