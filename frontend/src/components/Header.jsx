9ed5b275bb90ebe94cd7cc5033b320913e834b74
Josevan Oliveira <josivantarcio@msn.com>
2024-08-14 10:15:41 -0300

    custom bootstrap theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,30 @@
-import { Navbar, Nav, Container} from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
-        <Navbar bg="dark" variant='dark' expand='md' collapseOnSelect>
-            <Container>
-                <Navbar.Brand href='/'>Test-shop</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                <Navbar.Collapse id='baisc-navbar-nav'/>
-                    <Nav className='ms-auto'>
-                        <Nav.Link href='/cart'><FaShoppingCart /> Carrinho</Nav.Link>
-                        <Nav.Link href='/login'><FaUser /> Entrar</Nav.Link> 
-                    </Nav>
-            </Container>
-        </Navbar>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Test-shop"/>
+            Test-shop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="baisc-navbar-nav" />
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart">
+              <FaShoppingCart /> Carrinho
+            </Nav.Link>
+            <Nav.Link href="/login">
+              <FaUser /> Entrar
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
