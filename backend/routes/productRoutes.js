ab79f28c0e82e955d4fe0e2d1a59fd26d7100910
Josevan Oliveira <josivantarcio@msn.com>
2024-08-16 11:12:17 -0300

    personalizando erro Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,21 +4,28 @@ const router = express.Router();
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
-router.get("/", asyncHandler(async (req, res) => {
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
   })
 );
 
-router.get("/:id", asyncHandler(async (req, res) => {
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     //const product = products.find((p) => p._id === req.params.id);
 
-    if(product){
-        res.json(product);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Recurso não encontrado");
     }
 
-    res.status(404).json({ message: "Produto não encontrado"});
+    res.status(404).json({ message: "Produto não encontrado" });
   })
 );
 
