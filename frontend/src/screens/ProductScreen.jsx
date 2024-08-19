882decf541d4556fd0273cf8ade525c608b66e8b
Josevan Oliveira <josivantarcio@msn.com>
2024-08-19 20:22:08 -0300

    loader & message components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,99 +1,95 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-//import products from "../products";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState({});
-
   const { id: productId } = useParams();
-  //const product = products.find((p) => p._id === productId);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      console.log('buscando produto...');
-  
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    };
-  
-    fetchProduct();
-  }, [productId]);
-  
-  if (!product || !product._id) {  // Ajuste aqui
-    return <div>Produto não encontrado</div>;
-  }
-  
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
         Voltar
       </Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} avaliações`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Preço: R${product.price.toFixed(2).replace(".", ",")}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <strong>Descrição:</strong> {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Preço:</Col>
-                  <Col>
-                    <strong>
-                      R${product.price.toFixed(2).replace(".", ",")}
-                    </strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0
-                        ? "Em Estoque"
-                        : "Fora de Estoque"}
-                    </strong>
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} avaliações`}
+                />
               </ListGroup.Item>
-              <ListGroup.Item className="text-center">
-                <Button
-                  className="btn-block"
-                  type="button"
-                  disabled={product.countInStock <= 0}
-                >
-                  Incluir no Carrinho
-                </Button>
+              <ListGroup.Item>
+                Preço: R${product.price.toFixed(2).replace(".", ",")}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <strong>Descrição:</strong> {product.description}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Preço:</Col>
+                    <Col>
+                      <strong>
+                        R${product.price.toFixed(2).replace(".", ",")}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0
+                          ? "Em Estoque"
+                          : "Fora de Estoque"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className="text-center">
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock <= 0}
+                  >
+                    Incluir no Carrinho
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
