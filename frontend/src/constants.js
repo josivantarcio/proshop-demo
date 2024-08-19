ddfe3d56a258921b67ec184ee373c01c9e8e11a7
Josevan Oliveira <josivantarcio@msn.com>
2024-08-19 19:30:03 -0300

    apiProductsSlice

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,10 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+//export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const BASE_URL = '';
+export const API_KEY = 'your_api_key';
+export const PAYPAL_CLIENT_ID = 'your_paypal_client_id';
+export const PAYPAL_CLIENT_SECRET = 'your_paypal_client_secret';
+export const PAYPAL_MODE = 'sandbox'; // 'sandbox' or 'live'
+export const PAYPAL_SANDBOX_API_URL = 'https://api.sandbox.paypal.com/v2';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,37 +1,28 @@
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
-//import products from "../products";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const { data } = await axios.get("/api/products");
-        console.log(data); // Verifique se os dados estão corretos
-        setProducts(data);
-      } catch (error) {
-        console.error("Erro ao buscar produtos:", error);
-      }
-    };
-  
-    fetchProducts();
-  }, []);
-  
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Últimos Produtos</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Carregando...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Últimos Produtos</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
