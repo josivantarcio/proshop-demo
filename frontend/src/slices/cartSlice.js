3d67ee9dcc64a73d6b046e8249ed2ebc7d0aa2dc
Josevan Oliveira <josivantarcio@msn.com>
2024-10-05 19:52:59 -0300

    Order API Slice & Start Order Screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -37,6 +37,12 @@ const cartSlice = createSlice({
       state.paymentMethod = action.payload;
       return updateCart(state);
     },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      // state.shippingAddress = {};
+      // state.paymentMethod = "PayPal";
+      return updateCart(state);
+    },
   },
 });
 
@@ -45,6 +51,7 @@ export const {
   removeFromCart,
   saveShippingAddress,
   savePaymentMethod,
+  clearCartItems,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
