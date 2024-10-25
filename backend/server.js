276a52669942eecf4bf9433f6702a45f520b2302
Josevan Oliveira <josivantarcio@msn.com>
2024-10-25 10:26:53 -0300

    PayPal Setup & Order Paid - fake

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Definir rotas e middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 //Cookie parser middleware
 app.use(cookieParser());
@@ -24,9 +24,13 @@ app.get("/", (req, res) => {
   res.send("API em execução...");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("api/config/paypal", (res, req) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 app.use(notFound);
 app.use(errorHandler);
